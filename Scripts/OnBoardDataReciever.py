9d8c3157c4aff8f6c17e99c509828d46c4a04b81
Regoczi Aron <regocziaron@gmail.com>
2026-02-05 22:18:29 +0100

    Add OnBoardDataReceiver for serial data handling

---
+++ Scripts/OnBoardDataReciever.py
@@ -0,0 +1,138 @@
+import serial, struct, binascii
+
+LOG_FORMAT = "<IiiihHHB"
+RECORD_SIZE = struct.calcsize(LOG_FORMAT)
+
+def receiveFiles():
+    try:
+        while ser.read(2) != b"\xAA\x55":
+            pass
+    except NameError:
+        readFile()
+        return None
+    
+    fileCount = struct.unpack("<H", ser.read(2))[0]
+
+    allData = {}
+    
+    for _ in range(fileCount):
+        nameLen = struct.unpack("<B", ser.read(1))[0]
+        fname = ser.read(nameLen).decode()
+        
+        size = struct.unpack("<I", ser.read(4))[0]
+        data = ser.read(size)
+        
+        crc_recv = struct.unpack("<H", ser.read(2))[0]
+        crc_calc = binascii.crc32(data) & 0xFFFF
+
+        if crc_recv != crc_calc:
+            raise ValueError("CRC mismatch")
+        
+        records = []
+
+        for i in range(0, len(data), RECORD_SIZE):
+            records.append(struct.unpack(LOG_FORMAT, data[i:i+RECORD_SIZE]))
+        
+        allData[fname] = records
+
+    return allData
+
+def createDatabase(data):
+    database = {}
+    for rname, r in data.items():
+        db = {
+            "time": [],
+            "lat": [],
+            "lon": [],
+            "alt": [],
+            "temp": [],
+            "pressure": [],
+            "humidity": [],
+            "gpsValid": []
+        }
+        for l in r:
+            ts, lat, lon, alt, temp, pres, hum, valid = l
+
+            db["time"].append(ts)
+            db["lat"].append(lat / 1e6)
+            db["lon"].append(lon / 1e6)
+            db["alt"].append(alt / 10)
+            db["temp"].append(temp / 100)
+            db["pressure"].append(pres / 10)
+            db["humidity"].append(hum / 100)
+            db["gpsValid"].append(bool(valid))
+
+        database[rname] = db
+
+    with open("OnBoardData.txt", "w") as f:
+        for rname, records in database.items():
+            f.write(f"{rname}\n")
+            for i, rec in enumerate(records["time"]):
+                f.write(f"{records['time'][i]},{records['lat'][i]},{records['lon'][i]},{records['alt'][i]},{records['temp'][i]},{records['pressure'][i]},{records['humidity'][i]},{records['gpsValid'][i]}\n")
+
+    return database
+
+def printFile(fname):
+    db = database[fname]
+    n = len(db["time"])
+
+    print("Time        Latitude     Longitude   Altitude    Temperature   Pressure      Humidity     GPS Valid")
+
+    for i in range(n):
+        print(f"{db['time'][i]:.0f}   {db['lat'][i]:.6f}    {db['lon'][i]:.6f}    {db['alt'][i]:.1f}        {db['temp'][i]:.2f}        {db['pressure'][i]:.1f}        {db['humidity'][i]:.2f}        {db['gpsValid'][i]}")
+        print()
+
+def readFile():
+    with open("OnBoardData.txt", "r") as f:
+        data = f.read()
+        data = data.split("\n")
+        database = {}
+
+        for l in data:
+            if l == "":
+                continue
+            if l.endswith(".bin"):
+                name = l
+                database[name] = {
+                    "time": [],
+                    "lat": [],
+                    "lon": [],
+                    "alt": [],
+                    "temp": [],
+                    "pressure": [],
+                    "humidity": [],
+                    "gpsValid": []
+                }
+            else:
+                time, lat, lon, alt, temp, pres, hum, valid = l.split(",")
+                database[name]["time"].append(float(time))
+                database[name]["lat"].append(float(lat))
+                database[name]["lon"].append(float(lon))
+                database[name]["alt"].append(float(alt))
+                database[name]["temp"].append(float(temp))
+                database[name]["pressure"].append(float(pres))
+                database[name]["humidity"].append(float(hum))
+                database[name]["gpsValid"].append(bool(valid))
+
+    return database
+
+try:
+    ser = serial.Serial("COM5", 115200, timeout=20)
+except serial.SerialException:
+    print("Serial port not found, reading file")
+
+
+allBinary = receiveFiles()
+
+if allBinary != None:
+    database = createDatabase(allBinary)
+else:
+    database = readFile()
+
+for logName, log in database.items():
+    printName = logName.replace("log_", "").replace(".bin", "").replace("0", "")
+    print(printName)
+print("\nWhich file would you like to open?")
+openfile = int(input(""))
+actualOpenFile = f"log_{openfile:04d}.bin"
+printFile(actualOpenFile)
